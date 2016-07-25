a803529d1a29f09e399ddf88172ede813758f01b
Tim Turner <timdturner@gmail.com>
2016-07-25 16:04:49 -0400

    Add basic UserController

---
+++ src/app/users/UserController.ts
@@ -0,0 +1,27 @@
+import * as express from 'express';
+import {UserModel} from './UserModel';
+import environment from '../../environment';
+
+export class UserController {
+    public static getUser(req: express.Request, res: express.Response) {
+        UserModel.findById(req.user).exec()
+        .then(user => {
+            if(!user) {
+                return res.send({ message: 'User not found' });
+            }
+            res.send(user);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+
+    public static updateUser(req: express.Request, res: express.Response) {
+        UserModel.findByIdAndUpdate(req.user, req.body).exec();
+    }
+
+    public static deleteUser(req: express.Request, res: express.Response) {
+        UserModel.findByIdAndRemove(req.user).exec();
+    }
+
+}
