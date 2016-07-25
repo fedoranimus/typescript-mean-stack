91cce02e9c32706f9339dbb4d28a485e1107c6e4
Tim Turner <timdturner@gmail.com>
2016-07-25 16:06:27 -0400

    Clean up type issues

--- src/app/users/UserController.ts
+++ src/app/users/UserController.ts
@@ -4,7 +4,7 @@ import environment from '../../environment';
 
 export class UserController {
     public static getUser(req: express.Request, res: express.Response) {
-        UserModel.findById(req.user).exec()
+        UserModel.findById(req['user']).exec()
         .then(user => {
             if(!user) {
                 return res.send({ message: 'User not found' });
@@ -17,11 +17,11 @@ export class UserController {
     }
 
     public static updateUser(req: express.Request, res: express.Response) {
-        UserModel.findByIdAndUpdate(req.user, req.body).exec();
+        UserModel.findByIdAndUpdate(req['user'], req.body).exec();
     }
 
     public static deleteUser(req: express.Request, res: express.Response) {
-        UserModel.findByIdAndRemove(req.user).exec();
+        UserModel.findByIdAndRemove(req['user']).exec();
     }
 
 }
